b1594f2ab657784fe323db675e5fc73950146e1a
nikpapage <thitsa@ip-10-96-0-90.us-west-2.compute.internal>
2020-06-25 14:39:55 +0000

    server

--- server.js
+++ server.js
@@ -7,17 +7,25 @@ var ssh = require('./ssh');
 
 app.post('/echo', function (request, response) {
     console.log("Step 1:----Received Request---");
-     var data ='Hello World'
-     console.log("Step 2:---Preparing Internal HTTP Exit Call---")
-     //Async Exit call to capture everything that goes on on SSH2SHELL
-      
-     console.log("Step 3:---Preparing Downstram(SSH2SHELL) Correlation Header---");
-     var asyncExitCall= appdynamics.getAsyncCorrelation(request);
-     console.log("Step 4:---Firing SSH Command");
-     var txn =  appdynamics.appdynamics.getTransaction(request);
-     ssh.executeMultipleCommands(asyncExitCall,txn);
+
+    var body = [];
+    request.on('data', (chunk) => {
+      body.push(chunk);
+    }).on('end', () => {
+    	 data = Buffer.concat(body).toString();
+    	 console.log("----Message-----");
+   	  console.log(data)
+
+    	 console.log("Step 2:---Preparing Internal HTTP Exit Call---")
+    	 //Async Exit call to capture everything that goes on on SSH2SHELL
+   	  console.log("Step 3:---Preparing Downstram(SSH2SHELL)---");
+    	 var asyncExitCall= appdynamics.getAsyncCorrelation(request);
+    	 console.log("Step 4:---Firing SSH Command");
+    	 var txn =  appdynamics.appdynamics.getTransaction(request);
+    	 ssh.executeMultipleCommands(asyncExitCall,txn,data);
      
-     response.send('Hello World!');
+    	 response.send('Hello World!');
+    });
 });
 app.listen(3000, function(){
 	console.log('App Listening to port 3000');
